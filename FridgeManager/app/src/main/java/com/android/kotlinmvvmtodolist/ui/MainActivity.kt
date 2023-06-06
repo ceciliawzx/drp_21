cf3a3639bc12c5694cddf62a4deaa022ffa615d7
Cecilia Wang <ceciliawang@Cecilias-Mac.local>
2023-06-06 16:54:54 +0100

    [zw4021] add bottom navigation bar

--- FridgeManager/app/src/main/java/com/android/kotlinmvvmtodolist/ui/MainActivity.kt
+++ FridgeManager/app/src/main/java/com/android/kotlinmvvmtodolist/ui/MainActivity.kt
@@ -3,22 +3,43 @@ package com.android.kotlinmvvmtodolist.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.android.kotlinmvvmtodolist.R
 import com.android.kotlinmvvmtodolist.ui.task.TaskViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val taskViewModel: TaskViewModel by viewModels()
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+        navController = findNavController(R.id.nav_host_fragment)
+        bottomNavigationView = findViewById(R.id.bottom_bar)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_storage -> {
+                    navController.navigate(R.id.taskFragment)
+                    true
+                }
+                R.id.action_shopping_cart -> {
+                    // Navigate to the shopping cart fragment
+                    // Replace "YourShoppingCartFragment" with the actual fragment you want to navigate to
+                    navController.navigate(R.id.addFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
