4bd769e4753ae6daa2c47d0cdea395c724f85a8c
Cecilia Wang <ceciliawang@Cecilias-Mac.local>
2023-06-07 20:29:45 +0100

    [zw4021] fixed bugs in barcode scanner

--- FridgeManager/app/src/main/java/com/android/kotlinmvvmtodolist/ui/task/TaskFragment.kt
+++ FridgeManager/app/src/main/java/com/android/kotlinmvvmtodolist/ui/task/TaskFragment.kt
@@ -30,6 +30,7 @@ import com.android.kotlinmvvmtodolist.databinding.FragmentTaskBinding
 import com.android.kotlinmvvmtodolist.ui.add.calculateDaysLeft
 import com.android.kotlinmvvmtodolist.ui.add.getNotificationTime
 import com.android.kotlinmvvmtodolist.ui.add.scheduleNotification
+import com.android.kotlinmvvmtodolist.ui.add.showAlert
 import com.android.kotlinmvvmtodolist.util.TrustAllCerts
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -243,6 +244,10 @@ class TaskFragment : Fragment() {
             } catch (_: Exception) {
 
             }
+        } else {
+            requireActivity().runOnUiThread {
+                Toast.makeText(binding.root.context, "Scan failed", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -272,81 +277,121 @@ class TaskFragment : Fragment() {
 
             override fun onResponse(call: Call, response: Response) {
                 Log.d("Requesting", "Requesting http request")
+
                 val responseBody = response.body?.string()
+                var product: JSONObject? = null
+                try {
+                    product = responseBody?.let { parseProductFromJson(it) }
+                } catch (_: java.lang.Exception) {
+                    Log.d("Requesting", "product is null, not found")
+                }
+
+
                 // Process the response body
-                if (response.isSuccessful && responseBody != null) {
+                if (response.isSuccessful && responseBody != null && product != null) {
                     // Parse the response JSON
-                    val product = parseProductFromJson(responseBody)
+                    Log.d("Requesting", "product = $product")
 
-
-//                    val failed = product?.getString("product not found") != null
-                    // Retrieve the food name
-                    val productName: String = try {
-                        product?.getString("product_name") ?: ""
-                    } catch (e: JSONException) {
-                        ""
-                    }
-
-                    // Retrieve the serving quantity
-
-                    val productAmount: Int = try {
-                        product?.getString("product_quantity")?.toInt() ?: 1
-                    } catch (e: JSONException) {
-                        1
-                    }
-
-                    val productUnit: String = try {
-                        product?.getString("quantity") ?: ""
-                    } catch (e: JSONException) {
-                        ""
-                    }
-
-                    val number = productUnit.split(Regex("\\D+"))
-                    val unit = productUnit.split(Regex("\\d+")).get(0)
-
-                    val expirationDateString: String = try {
-                        product?.getString("expiration_date") ?: ""
-                    } catch (e: JSONException) {
-                        ""
-                    }
-
-                    val expirationDate: String
-                    if (expirationDateString != "") {
-                        Log.d("Requesting", "default date problem")
-                        val day = expirationDateString.split(' ')[0]
-                        val month: Int = when (expirationDateString.split(' ')[1]) {
-                            "Jan" -> 1
-                            "Feb" -> 2
-                            "Mar" -> 3
-                            "Apr" -> 4
-                            "May" -> 5
-                            "Jun" -> 6
-                            "Jul" -> 7
-                            "Aug" -> 8
-                            "Sep" -> 9
-                            "Oct" -> 10
-                            "Nov" -> 11
-                            "Dec" -> 12
-                            else -> 0
+                    try {
+                        // Retrieve the food name
+                        val productName: String = try {
+                            product?.getString("product_name") ?: ""
+                        } catch (e: JSONException) {
+                            ""
                         }
-                        val year = expirationDateString.split(' ')[2]
-                        expirationDate = "$year-$month-$day"
-                    } else {
+
+                        // Retrieve the serving quantity
+
+                        val productAmount: Int = try {
+                            product?.getString("product_quantity")?.toInt() ?: 1
+                        } catch (e: JSONException) {
+                            1
+                        }
+
+                        val productUnit: String = try {
+                            product?.getString("quantity") ?: ""
+                        } catch (e: JSONException) {
+                            ""
+                        }
+
+                        // TODO: extract unit
+                        val number = productUnit.split(Regex("\\D+"))
+                        val unit = productUnit.split(Regex("\\d+"))[0]
+
+                        // Retrieve expiration date
+                        val expirationDateString: String = try {
+                            product?.getString("expiration_date") ?: ""
+                        } catch (e: JSONException) {
+                            ""
+                        }
+
+                        // TODO: handle expiration date formats
                         val defaultDateString = LocalDate.now().plusDays(1)
                         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-                        expirationDate = defaultDateString.format(formatter)
-                    }
+                        var expirationDate = defaultDateString.format(formatter)
+                        Log.d("Requesting", "default = $expirationDate")
 
-                    val notificationID = viewModel.getNextNotificationID()
+                        if (expirationDateString != "") {
+                            Log.d("Requesting", "dateString = $expirationDateString")
+                            try {
+                                var year: String = ""
+                                var month: Int = 0
+                                var day: String = ""
 
-                    fail = product == null || productName == "" || expirationDate == ""
+                                var times = expirationDateString.split(' ')
+                                // case 2 eg: 21 Jul 2023
+                                if (times.size == 3) {
+                                    day = times[0]
+                                    month = when (times[1]) {
+                                        "Jan" -> 1
+                                        "Feb" -> 2
+                                        "Mar" -> 3
+                                        "Apr" -> 4
+                                        "May" -> 5
+                                        "Jun" -> 6
+                                        "Jul" -> 7
+                                        "Aug" -> 8
+                                        "Sep" -> 9
+                                        "Oct" -> 10
+                                        "Nov" -> 11
+                                        "Dec" -> 12
+                                        else -> 0
+                                    }
+                                    year = expirationDateString.split(' ')[2]
+                                    expirationDate = "$year-$month-$day"
+                                }
 
-                    if (fail) {
-                        Toast.makeText(requireContext(), "Scan failed", Toast.LENGTH_SHORT).show()
-                        // TODO: handle the failure
-                        Log.d("Requesting", "fail because of some contents")
-                    } else {
-                        Log.d("Requesting", "contents success, other reasons")
+                                times = expirationDateString.split('/')
+                                if (times.size == 3) {
+                                    year = times[0]
+                                    if (year.length == 2) year = "20$year"
+                                    month = times[1].toInt()
+                                    day = times[2]
+                                    expirationDate = "$year-$month-$day"
+                                }
+
+                            } catch (_: java.lang.Exception) {
+                                // handle the expiration date exception
+                                Log.d("Requesting", "dateString = $expirationDateString")
+                            }
+                        } else {
+                            Log.d("Requesting", "dateString = null? $expirationDateString")
+                        }
+
+                        val notificationID = viewModel.getNextNotificationID()
+
+                        fail = productName == "" || expirationDate == ""
+
+                        if (fail) {
+                            requireActivity().runOnUiThread {
+                                Toast.makeText(requireActivity(), "Scan failed", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                            // TODO: handle the failure
+                            Log.d("Requesting", "fail because of some contents")
+                            return
+                        } else {
+                            Log.d("Requesting", "contents success, other reasons")
                             val taskEntry = TaskEntry(
                                 0,
                                 productName,
@@ -369,16 +414,27 @@ class TaskFragment : Fragment() {
                             // TODO: notify ? days before expiration
                             val message1 = "Your $productName will expire in $daysLeft days!"
                             val message = "Your $productName will expire tomorrow!!!"
-                            scheduleNotification(requireContext(), title, message, notificationTime, notificationID)
-                            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
+                            scheduleNotification(binding.root.context, title, message, notificationTime, notificationID)
+                            requireActivity().runOnUiThread {
+                                showAlert(notificationTime, title, message, binding.root.context)
+                            }
+                            requireActivity().runOnUiThread {
+                                Toast.makeText(binding.root.context, "Successfully Added!", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    } catch (_: java.lang.Exception) {
+                        requireActivity().runOnUiThread {
+                            Toast.makeText(binding.root.context, "Product not found", Toast.LENGTH_SHORT).show()
+                        }
                     }
                 } else {
-                    Log.d("Requesting", "http request is failed")
-                    Toast.makeText(requireContext(), "Scan failed", Toast.LENGTH_SHORT).show()
+                    Log.d("Requesting", "Product not found")
+                    requireActivity().runOnUiThread {
+                        Toast.makeText(binding.root.context, "Product not found", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
-        }
-        )
+        })
     }
 
     private fun parseProductFromJson(json: String): JSONObject? {
