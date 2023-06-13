9451c857caa70bcc3bb0493d8b9d3b6dd36840f0
murixiyang <yangqinxue333@163.com>
2023-06-13 14:53:40 +0100

    Can retrieve message list, can add to message list

--- FridgeManager/app/src/main/java/com/android/kotlinmvvmtodolist/ui/chat/ConversationFragment.kt
+++ FridgeManager/app/src/main/java/com/android/kotlinmvvmtodolist/ui/chat/ConversationFragment.kt
@@ -9,11 +9,13 @@ import android.widget.EditText
 import android.widget.ImageView
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.RecyclerView
-import com.android.kotlinmvvmtodolist.R
-import com.android.kotlinmvvmtodolist.databinding.FragmentChatBinding
 import com.android.kotlinmvvmtodolist.databinding.FragmentConversationBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.android.kotlinmvvmtodolist.util.Constants.USER_DATABASE_REFERENCE
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+
 
 class ConversationFragment : Fragment() {
 
@@ -22,10 +24,12 @@ class ConversationFragment : Fragment() {
     private lateinit var sendButton: ImageView
     private val args by navArgs<ConversationFragmentArgs>()
     private lateinit var oppUid: String
-    private val myUid = FirebaseAuth.getInstance().currentUser?.uid
+    private val myUid = FirebaseAuth.getInstance().currentUser?.uid!!
 
     private var _binding: FragmentConversationBinding? = null
     private val binding get() = _binding!!
+
+    val messageList: MutableList<Message> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +40,7 @@ class ConversationFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentConversationBinding.inflate(inflater, container, false)
 
@@ -44,19 +48,57 @@ class ConversationFragment : Fragment() {
         messageBox = binding.messageBox
         sendButton = binding.sendButton
 
+        val messageRef = USER_DATABASE_REFERENCE
+            .child("User").child(myUid)
+            .child("Contacts").child(oppUid)
+            .child("Message")
+
+        val messageListener = messageRef.addListenerForSingleValueEvent(
+            (object : ValueEventListener {
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    // Clear to fetch
+                    messageList.clear()
+
+                    // Add new version of message list
+                    for (childSnapshot in dataSnapshot.children) {
+                        val message = childSnapshot.getValue(Message::class.java)
+                        message?.let { messageList.add(it) }
+                    }
+
+                    println("this is lydia")
+
+                    messageList.forEach { message ->
+                        println(message.message)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+        )
+
         sendButton.setOnClickListener {
-            if (myUid != null) {
 
-                val newMessage = listOf(Message(messageBox.text.toString(), myUid), Message("Hello Bob", myUid))
+            // Add new message to origin list
+            val newMessage = Message(messageBox.text.toString(), myUid)
+            messageList.add(newMessage)
 
-                USER_DATABASE_REFERENCE
-                    .child("User").child(myUid)
-                    .child("Contacts").child(oppUid)
-                    .child("Message").setValue(newMessage)
-            }
+            // set new message list
+            USER_DATABASE_REFERENCE
+                .child("User").child(myUid)
+                .child("Contacts").child(oppUid)
+                .child("Message").setValue(messageList)
         }
 
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
